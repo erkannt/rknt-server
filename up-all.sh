1a8618e1a1f59e8d60dfa3f874b375e255c67532
Daniel Haarhoff <haarhoff@mailbox.org>
2020-05-23 21:07:33 +0100

    feat(up-all): print full command

--- up-all.sh
+++ up-all.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 for composefile in $(find . -name docker-compose.yml); do
-  echo $composefile
+  echo docker-compose -f $composefile up -d --remove-orphans
   docker-compose -f $composefile up -d --remove-orphans
 done
