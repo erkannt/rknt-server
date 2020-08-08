6ea80af651b28c1941df6d2875223c42d631b701
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-08-08 15:41:03 +0100

    feat(ci): add container garbage collection

---
+++ garbage_collect.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+defined_containers=""
+for composefile in $(find . -name docker-compose.yml); do
+    service=$(echo $composefile | cut -f 2 -d '/')
+    containers=$(yq read ${composefile} --printMode p 'services.*' | sed -e 's/services\.//')
+    for container in $containers; do
+        defined_containers="${defined_containers} ${service}_${container}_1"
+    done
+done
+
+running_containers=$(docker ps --format '{{.Names}}')
+
+for container in ${running_containers}; do
+    case "${container}" in *${defined_containers})
+        echo ${container} "is defined"
+        ;;
+        *)
+        echo ${container} "not defined -- deleting"
+        docker kill ${container}
+        ;;
+    esac
+done
