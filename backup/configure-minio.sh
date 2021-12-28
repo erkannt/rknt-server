dfebe92f0a91f1b2354090f46384055f3920b8ec
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-12-28 17:40:49 +0000

    fix(backup): admin policy already exists, readonly list policy needed

--- backup/configure-minio.sh
+++ backup/configure-minio.sh
@@ -3,10 +3,10 @@ source ../.env
 
 mc alias set backup https://backup.rknt.de ${BACKUP_ACCESS_KEY} ${BACKUP_SECRET_KEY}
 
-mc admin policy add backup consoleAdmin admin-policy.json
+mc admin policy add backup readonlyWithList readonlyWithList.json
 
 mc admin user add backup ${BACKUP_READ_ACCESS_KEY} ${BACKUP_READ_SECRET_KEY}
-mc admin policy set backup readwrite user=${BACKUP_READ_ACCESS_KEY}
+mc admin policy set backup readonlyWithList user=${BACKUP_READ_ACCESS_KEY}
 
 mc admin user add backup ${BACKUP_ADMIN_ACCESS_KEY} ${BACKUP_ADMIN_SECRET_KEY}
 mc admin policy set backup consoleAdmin user=${BACKUP_ADMIN_ACCESS_KEY}
